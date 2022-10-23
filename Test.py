d85ca772eb14588f87716dcca3ca15411c2bdccf
Viktor Kakorin <vkakorin@gmail.com>
2022-10-23 09:32:08 +0300

    Testing with selenium

---
+++ Test.py
@@ -0,0 +1,38 @@
+import math
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+    # Calculating answer
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "[for='robotCheckbox']")
+    option1.click()
+
+    option2 = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
+    option2.click()
+
+    # Submitting form
+    button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-default")
+    button.click()
+
+finally:
+    # Time to see answer
+    time.sleep(10)
+    browser.quit()
